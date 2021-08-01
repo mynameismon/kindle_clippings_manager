3ad6f83ec3f8b4e37f3bc96c765eea4bb55f4465
mynameismon <programminggeekishere@gmail.com>
2021-08-01 17:20:18 +0530

    fix issues with database structure and sql commands
    1. changed query execution from deprecated QSqlDatabase.exec() to
       QSqlQuery.exec()
       refer:
       https://www.riverbankcomputing.com/static/Docs/PyQt6/api/qtsql/qsqldatabase.html#:~:text=Note:%20exec()
    
    2. Changed
        i. book to integer to align with the foreign key
        ii. date to datetime

--- src/database.py
+++ src/database.py
@@ -1,6 +1,6 @@
 from PyQt6 import QtSql
 from PyQt6.QtWidgets import QMessageBox
-from PyQt6.QtSql import QSqlDatabase
+from PyQt6.QtSql import QSqlDatabase, QSqlQuery
 
 INIT_QUERY = (
 """
@@ -11,19 +11,19 @@ CREATE TABLE IF NOT EXISTS clippingtypes (
 """
 CREATE TABLE IF NOT EXISTS books (
     book_id INTEGER PRIMARY KEY AUTOINCREMENT UNIQUE NOT NULL,
-    book_title TEXT UNIQUE,
+    book_title TEXT UNIQUE NOT NULL,
     author TEXT
 );""",
 """
 CREATE TABLE IF NOT EXISTS clippings (
     id INTEGER PRIMARY KEY AUTOINCREMENT UNIQUE NOT NULL,
-    book TEXT NOT NULL,
+    book INTEGER NOT NULL,
     author TEXT,
     clipping TEXT UNIQUE NOT NULL,
     type CHAR(1) NOT NULL REFERENCES clippingtypes(type),
     start_location INTEGER NOT NULL,
     end_location INTEGER NOT NULL,
-    date DATE,
+    date DATETIME,
 
     FOREIGN KEY (book) REFERENCES books(book_id)
 );
